7b8d31dd29e8bcd87663c17b466e0aa5bb23eae9
Alexis <a.beingessner@gmail.com>
2015-02-10 18:48:04 -0500

    update to rust nightly, add in experimental const'd btree

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+ #![feature(collections, test, os, path, io, core)]
+ #![allow(deprecated, unused_imports)]
+
 extern crate collections;
 extern crate test;
+extern crate collect;
+extern crate btree;
 
-use std::collections::{RingBuf, BTreeSet, TreeSet};
-use std::io;
+use std::collections::{RingBuf, BTreeSet};
+use collect::TreeSet;
+use btree::set::BTreeSet as BTreeSet2;
+use std::old_io;
 use std::os;
 use std::rc::Rc;
 
@@ -10,11 +17,11 @@ type Set = TreeSet<Rc<String>>;
 
 struct History {
     q: RingBuf<Rc<String>>,
-    max_size: uint,
+    max_size: usize,
 }
 
 impl History {
-    fn new(max_size: uint) -> History {
+    fn new(max_size: usize) -> History {
         History {
             q: RingBuf::with_capacity(max_size),
             max_size: max_size,
@@ -47,18 +54,19 @@ fn contains(map: &Set, e: &Rc<String>) -> bool {
     map.contains(e)
 }
 
-const HISTORY_SIZE: uint = 409600;
-const CONTAINS_LOOPS: uint = 2;
+const HISTORY_SIZE: usize = 409600;
+const CONTAINS_LOOPS: usize = 2;
 
 fn main() {
-    let mut f = io::BufferedReader::new(io::File::open(&Path::new(os::args()[1].clone())).unwrap());
+    let mut f = old_io::BufferedReader::new(old_io::File::open(&Path::new(os::args()[1].clone())).unwrap());
 
     let mut map = TreeSet::new();
     //let mut map = BTreeSet::new();
+
     // TODO: all of these will be in cache.
     let mut hist = History::new(HISTORY_SIZE);
 
-    let mut x = 0i;
+    let mut x = 0is;
 
     for line in f.lines() {
         for word in line.unwrap().words() {
